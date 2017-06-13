3c9c34fb1fce7308c9387ed234cb5cb520e3df3d
GiBg1aN <bigagliagianluca@gmail.com>
2017-06-13 22:26:56 +0200

    Basic game logic added

--- main.elm
+++ main.elm
@@ -17,7 +17,13 @@ type alias Model =
     { board : Board
     , status : Status
     , snake : Snake
+    , lastMove : Direction
     }
+
+
+type Cell
+    = Present
+    | Absent
 
 
 type alias Board =
@@ -29,11 +35,6 @@ type Status
     | Lost
 
 
-type Cell
-    = Present
-    | Absent
-
-
 type alias Snake =
     List Location
 
@@ -43,6 +44,11 @@ type Direction
     | Down
     | Left
     | Right
+
+
+model : Model
+model =
+    { board = initMatrix 10 10, status = Moving, snake = initSnake 5, lastMove = Up }
 
 
 initMatrix : Int -> Int -> Matrix Cell
@@ -59,9 +65,54 @@ initSnake n =
     [ ( 3, 3 ), ( 3, 4 ), ( 3, 5 ) ]
 
 
-model : Model
-model =
-    { board = initMatrix 10 10, status = Moving, snake = initSnake 5 }
+updateSnake : Snake -> Direction -> Board -> Maybe Snake
+updateSnake l d m =
+    case List.head l of
+        Just x ->
+            let
+                ( i, j ) =
+                    x
+            in
+            let
+                newHead =
+                    case d of
+                        Up ->
+                            ( i + 1, j )
+
+                        Down ->
+                            ( i - 1, j )
+
+                        Left ->
+                            ( i, j - 1 )
+
+                        Right ->
+                            ( i, j + 1 )
+            in
+            case Matrix.get newHead m of
+                Just Present ->
+                    Just (newHead :: l)
+
+                Just Absent ->
+                    Just (newHead :: (l |> List.take (List.length l - 1)))
+
+                Nothing ->
+                    Nothing
+
+        Nothing ->
+            Nothing
+
+
+isFailed : Location -> Snake -> Bool
+isFailed c l =
+    case l of
+        x :: xs ->
+            if x == c then
+                True
+            else
+                isFailed c xs
+
+        [] ->
+            False
 
 
 
@@ -73,9 +124,34 @@ type Msg
     | Move Direction
 
 
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Reset ->
+            { model | board = initMatrix 10 10, status = Moving, snake = initSnake 5 }
 
--- update : Msg -> Model -> Model
--- update msg model =
+        Move move ->
+            let
+                new_snake =
+                    updateSnake model.snake move model.board
+            in
+            case new_snake of
+                Just l ->
+                    case l of
+                        x :: xs ->
+                            if isFailed x l then
+                                { model | snake = l, status = Lost }
+                            else
+                                { model | snake = l }
+
+                        _ ->
+                            model
+
+                Nothing ->
+                    model
+
+
+
 -- VIEW
 -- view : Model -> Html Msg
 -- view model =
