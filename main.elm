323434a16cd1c9f9eefc3f48d85be42de0094a81
GiBg1aN <bigagliagianluca@gmail.com>
2017-07-31 17:42:50 +0200

    Migrate to standard Elm workflow
    To handle user input with Keyboard.Extra module is necessary to
    switch from Model/View/Update workflow to Init/View/Update/Subscriptions
    workflow.

--- main.elm
+++ main.elm
@@ -4,9 +4,19 @@ import Matrix exposing (..)
 import Html exposing (..)
 import Html.Events exposing (..)
 import Html.Attributes exposing (..)
+import Keyboard.Extra exposing (Key, Direction(..), arrowsDirection)
+
 
 main : Program Never Model Msg
-main = Html.beginnerProgram { model = model, view = view, update = update }
+main =
+    Html.program
+        { init = model
+        , update = update
+        , view = view
+        , subscriptions = subscriptions
+        }
+
+
 --MODEL
 
 
@@ -14,7 +24,7 @@ type alias Model =
     { board : Board
     , status : Status
     , snake : Snake
-    , lastMove : Direction
+    , lastMove : List Key
     }
 
 
@@ -36,16 +46,14 @@ type alias Snake =
     List Location
 
 
-type Direction
-    = Up
-    | Down
-    | Left
-    | Right
+type Msg
+    = Reset
+    | KeyboardMsg Keyboard.Extra.Msg
 
 
-model : Model
+model : (Model, Cmd Msg)
 model =
-    { board = initMatrix 10 10, status = Moving, snake = initSnake 5, lastMove = Up }
+    ({ board = initMatrix 10 10, status = Moving, snake = initSnake 5, lastMove = [] }, Cmd.none)
 
 
 initMatrix : Int -> Int -> Matrix Cell
@@ -70,30 +78,35 @@ updateSnake l d m =
                 ( i, j ) =
                     x
             in
-            let
-                newHead =
-                    case d of
-                        Up ->
-                            ( (i - 1) % 10, j )
+                let
+                    newHead =
+                        case d of
+                            North ->
+                                ( (i - 1) % 10, j )
 
-                        Down ->
-                            ( (i + 1) % 10, j )
+                            South ->
+                                ( (i + 1) % 10, j )
 
-                        Left ->
-                            ( i, (j - 1) % 10 )
+                            West ->
+                                ( i, (j - 1) % 10 )
 
-                        Right ->
-                            ( i, (j + 1) % 10 )
-            in
-            case Matrix.get newHead m of
-                Just Present ->
-                    Just (newHead :: l)
+                            East ->
+                                ( i, (j + 1) % 10 )
 
-                Just Absent ->
-                    Just (newHead :: (l |> List.take (List.length l - 1)))
+                            NoDirection ->
+                                (i, j)
+                            _ ->
+                                Debug.crash "INVALID KEY"
+                in
+                    case Matrix.get newHead m of
+                        Just Present ->
+                            Just (newHead :: l)
 
-                Nothing ->
-                    Nothing
+                        Just Absent ->
+                            Just (newHead :: (l |> List.take (List.length l - 1)))
+
+                        Nothing ->
+                            Nothing
 
         Nothing ->
             Nothing
@@ -116,67 +129,81 @@ isFailed c l =
 -- UPDATE
 
 
-type Msg
-    = Reset
-    | Move Direction
-
-
-update : Msg -> Model -> Model
+update : Msg -> Model -> (Model,Cmd Msg)
 update msg model =
     case msg of
         Reset ->
-            { model | board = initMatrix 10 10, status = Moving, snake = initSnake 5 }
+            ({ model | board = initMatrix 10 10, status = Moving, snake = initSnake 5, lastMove = [] }, Cmd.none)
 
-        Move move ->
+        KeyboardMsg move ->
             let
+                dir =
+                    Keyboard.Extra.update move model.lastMove
+
                 new_snake =
-                    updateSnake model.snake move model.board
+                    updateSnake model.snake (Debug.log "prova" (Keyboard.Extra.arrowsDirection dir)) model.board
             in
-            case new_snake of
-                Just l ->
-                    case l of
-                        x :: xs ->
-                            if isFailed x l then
-                                { model | snake = l, status = Lost }
-                            else
-                                { model | snake = l }
+                case new_snake of
+                    Just l ->
+                        case l of
+                            x :: xs ->
+                                if isFailed x l then
+                                    ({ model | snake = l, status = Lost, lastMove = [] },Cmd.none)
+                                else
+                                    ({ model | snake = l, lastMove = dir },Cmd.none)
 
-                        _ ->
-                            model
+                            _ ->
+                                (model, Cmd.none)
 
-                Nothing ->
-                    model
+                    Nothing ->
+                        (model, Cmd.none)
 
 
 
 -- VIEW
+
+
 renderCell : Cell -> Html msg
 renderCell p =
     case p of
-    Present -> td [ style [ ("margin","5px"), ( "padding", "30px" ), ( "width", "5px" ), ( "height", "5px" ), ( "background-color", "black" )] ] []
-    Absent -> td [ style [ ("margin","5px"),( "padding", "30px" ), ( "width", "5px" ), ( "height", "5px" ), ( "background-color", "grey" ) ] ] []
+        Present ->
+            td [ style [ ( "margin", "5px" ), ( "padding", "30px" ), ( "width", "5px" ), ( "height", "5px" ), ( "background-color", "black" ) ] ] []
+
+        Absent ->
+            td [ style [ ( "margin", "5px" ), ( "padding", "30px" ), ( "width", "5px" ), ( "height", "5px" ), ( "background-color", "grey" ) ] ] []
 
 
 addSnake : Snake -> Board -> Board
 addSnake s m =
-    m |> Matrix.mapWithLocation (\lct c -> if List.member lct s then Present else Absent)
+    m
+        |> Matrix.mapWithLocation
+            (\lct c ->
+                if List.member lct s then
+                    Present
+                else
+                    Absent
+            )
 
 
 view : Model -> Html Msg
 view model =
-    let reset =
-        div [] [ button [ onClick Reset, class "btn" ] [ text "Reset" ] ]
-
-
+    let
+        reset =
+            div [] [ button [ onClick Reset, class "btn" ] [ text "Reset" ] ]
 
         cells =
-             model.board |> (addSnake model.snake) >> (Matrix.map (\c -> renderCell c)) >> Matrix.toList >> (List.map (\r -> tr [] r))
+            model.board |> (addSnake model.snake) >> (Matrix.map (\c -> renderCell c)) >> Matrix.toList >> (List.map (\r -> tr [] r))
 
         board =
             if model.status == Lost then
                 div [ style [ ( "display", "flex" ), ( "padding", "20px" ) ] ] [ h1 [] [ text "You Lost!" ] ]
             else
-                div [ style [ ( "display", "flex" ), ( "padding", "25px" ) ] ] [table [] cells]
+                div [ style [ ( "display", "flex" ), ( "padding", "25px" ) ] ] [ table [] cells ]
     in
-    div [] [ div [] [ h3 [ style [ ( "padding-bottom", "5px" ) ] ] [] ], board, reset ]
+        div [] [ div [] [ h3 [ style [ ( "padding-bottom", "5px" ) ] ] [] ], board, reset ]
 
+-- SUBSCRIPTIONS
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.map KeyboardMsg Keyboard.Extra.subscriptions
