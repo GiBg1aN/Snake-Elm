a401dd59a6d76face8b5ec1e2d687c7ac911ed94
GiBg1aN <bigagliagianluca@gmail.com>
2017-10-19 17:11:29 +0200

    Color victory/lost messages
    Now, victory message is in green and lost message is in red

--- src/view/MainContent.elm
+++ src/view/MainContent.elm
@@ -10,10 +10,10 @@ view : Model -> List (Html Msg) -> Html Msg
 view model cells =
     case model.status of
         Lost ->
-            div [ style [ ( "display", "flex" ), ( "padding", "200px" ) ] ] [ h1 [] [ text "You Lost!" ] ]
+            div [ style [ ( "display", "flex" ), ( "padding", "200px" ) ] ] [ h1 [ style [ ( "color", "red" ), ( "background-color", "white" ) ] ] [ text "You Lost!" ] ]
 
         Win ->
-            div [ style [ ( "display", "flex" ), ( "padding", "200px" ) ] ] [ h1 [] [ text "You Won!" ] ]
+            div [ style [ ( "display", "flex" ), ( "padding", "200px" ) ] ] [ h1 [ style [ ( "color", "green" ), ( "background-color", "white" ) ] ] [ text "You Won!" ] ]
 
         Moving ->
             div [ style [ ( "display", "flex" ) ] ] [ table [] cells ]
